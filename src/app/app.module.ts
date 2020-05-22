e717207a435e02d8e84655a8e3b1a0f174fc0d3a
Marvin Dawson <marvintdawson@gmail.com>
2020-05-21 23:22:54 -0500

    adding firestore and firebaseui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {LoginPage} from "../pages/login/login";
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    LoginPage,
+    TabsPage,
   ],
   imports: [
     BrowserModule,
@@ -29,6 +31,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    LoginPage,
     TabsPage
   ],
   providers: [
--- src/pages/login/login.module.ts
+++ src/pages/login/login.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { LoginPage } from './login';
 
+
 @NgModule({
   declarations: [
-    LoginPage,
+
   ],
   imports: [
     IonicPageModule.forChild(LoginPage),
   ],
 })
+// @ts-ignore
 export class LoginPageModule {}
